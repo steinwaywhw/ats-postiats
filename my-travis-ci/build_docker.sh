20330fe1e03b22330a3f2ce1e4a2d0265aff40c7
Hongwei Xi <gmhwxi@gmail.com>
2017-04-04 19:45:15 -0400

    Updating: very very minorly

--- my-travis-ci/build_docker.sh
+++ my-travis-ci/build_docker.sh
@@ -7,15 +7,15 @@ docker build -f Dockerfile -t $REPO:$TRAVIS_COMMIT --build-arg ATSVER=${ATSVER} 
 
 if [[ "$TRAVIS_BRANCH" == "master" ]]; then 
 	if [ -z "$TRAVIS_TAG" ]; then 
-		echo "$REPO:git"
-		docker tag $REPO:$TRAVIS_COMMIT $REPO:git                 # steinwaywhw/ats:git
-		docker push $REPO:git
+		echo "$REPO:github"
+		docker tag $REPO:$TRAVIS_COMMIT $REPO:github          # steinwaywhw/ats:github
+		docker push $REPO:github
 	else 
 		echo "$REPO:$TRAVIS_TAG"
-		docker tag $REPO:$TRAVIS_COMMIT $REPO:$TRAVIS_TAG         # steinwaywhw/ats:1.0
+		docker tag $REPO:$TRAVIS_COMMIT $REPO:$TRAVIS_TAG     # steinwaywhw/ats:1.0
 		docker push $REPO:$TRAVIS_TAG
 		echo "$REPO:latest"
-		docker tag $REPO:$TRAVIS_COMMIT $REPO:latest              # steinwaywhw/ats:latest
+		docker tag $REPO:$TRAVIS_COMMIT $REPO:latest          # steinwaywhw/ats:latest
 		docker push $REPO:latest
 	fi
 else
