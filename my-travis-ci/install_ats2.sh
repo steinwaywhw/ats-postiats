f50b9823e8e5a42195e7b76d705755cb443258e3
Hongwei Xi <gmhwxi@gmail.com>
2017-04-08 14:29:17 -0400

    Updating: very very minorly fixing

--- my-travis-ci/install_ats2.sh
+++ my-travis-ci/install_ats2.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env sh
-
-
+#
+make -f codegen/Makefile_atslib
+#
 make -f Makefile_devl CC=${CC} all
+#
 # make GCFLAG=-D_ATS_GCATS -f Makefile_devl CC=${GCC} all
 # make C3NSTRINTKND=intknd -f Makefile_devl CC=${CLANG} all
 # make C3NSTRINTKND=intknd GCFLAG=-D_ATS_GCATS -f Makefile_devl CC=${CLANG} all
-
+#
 make -C src CBOOT
 make -C src/CBOOT/prelude
 make -C src/CBOOT/libc
@@ -13,6 +15,10 @@ make -C src/CBOOT/libats
 make -C doc/DISTRIB atspackaging
 make -C doc/DISTRIB atspacktarzvcf
 make -C doc/DISTRIB cleanall
+#
 # make -C ${PATSHOME}/doc/DISTRIB atscontribing
 # make -C ${PATSHOME}/doc/DISTRIB atscntrbtarzvcf
 # make -C ${PATSHOME}/doc/DISTRIB cleanall
+#
+# end of [install_ats2.sh] #
+
