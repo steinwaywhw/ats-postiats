df6cb9f98b948a7e268e257bc0af4ad7b03c3b9d
Hanwen Wu <steinwaywhw@gmail.com>
2017-05-26 11:03:48 -0400

    update build script

--- my-travis-ci/install_ats2.sh
+++ my-travis-ci/install_ats2.sh
@@ -1,20 +1,19 @@
 #!/usr/bin/env sh
 #
-make -f codegen/Makefile_atslib
-#
+#make -f codegen/Makefile_atslib
+
+cp ${ATSHOME}/config.h .
 make -f Makefile_devl CC=${CC} all
 #
 # make GCFLAG=-D_ATS_GCATS -f Makefile_devl CC=${GCC} all
 # make C3NSTRINTKND=intknd -f Makefile_devl CC=${CLANG} all
 # make C3NSTRINTKND=intknd GCFLAG=-D_ATS_GCATS -f Makefile_devl CC=${CLANG} all
 #
-make -C src CBOOT
-make -C src/CBOOT/prelude
-make -C src/CBOOT/libc
-make -C src/CBOOT/libats
-make -C doc/DISTRIB atspackaging
-make -C doc/DISTRIB atspacktarzvcf
-make -C doc/DISTRIB cleanall
+
+
+# make -C doc/DISTRIB atspackaging
+# make -C doc/DISTRIB atspacktarzvcf
+# make -C doc/DISTRIB cleanall
 #
 # make -C ${PATSHOME}/doc/DISTRIB atscontribing
 # make -C ${PATSHOME}/doc/DISTRIB atscntrbtarzvcf
